8906de5789723c28ebad0dcc358619b357f28794
Aman Sheikh <haarishsheikh04@gmail.com>
2026-03-14 20:39:07 +0530

    Implement rate limit to booking API routes

--- src/routes/booking.route.ts
+++ src/routes/booking.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import {
-    cancelAdminBooking,
+  cancelAdminBooking,
   cancelBooking,
   createBooking,
   getAllBooking,
@@ -12,30 +12,48 @@ import {
 import { restrictTo } from "../lib/index.ts";
 import pkg from "express-openid-connect";
 import { bookingValidation } from "../middleware/validation.middleware.ts";
+import {
+  bookingRateLimit,
+  publicGetRateLimit,
+} from "../middleware/rate-limit.middleware.ts";
 
 const { requiresAuth } = pkg;
 
 const router = Router({ mergeParams: true });
 
-router.post("/", requiresAuth(), bookingValidation, createBooking);
-router.patch("/", requiresAuth(), cancelBooking);
-router.patch("/:bookingId", requiresAuth(), updateUserBooking);
-router.get("/user", requiresAuth(), getUserBookings);
+router.post(
+  "/",
+  requiresAuth(),
+  bookingRateLimit,
+  bookingValidation,
+  createBooking,
+);
+router.patch("/", requiresAuth(), bookingRateLimit, cancelBooking);
+router.patch(
+  "/:bookingId",
+  requiresAuth(),
+  bookingRateLimit,
+  updateUserBooking,
+);
+router.get("/user", requiresAuth(), publicGetRateLimit, getUserBookings);
 router.get(
   "/:propertyId",
   requiresAuth(),
+  publicGetRateLimit,
   restrictTo("Admin", "Super_Admin"),
   getBookingsForAdmin,
 );
 router.get(
   "/admin/all",
   requiresAuth(),
+  publicGetRateLimit,
   restrictTo("Super_Admin"),
   getAllBooking,
 );
 router.get(
   "/:bookingId",
   requiresAuth(),
+  publicGetRateLimit,
   restrictTo("Admin", "Super_Admin"),
   getBookingDetails,
 );
