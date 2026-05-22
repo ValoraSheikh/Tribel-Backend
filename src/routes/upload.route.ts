2661e0a74471c44a5dcc4e06f9e8a1df272c5281
Aman Sheikh <haarishsheikh04@gmail.com>
2026-05-22 12:41:19 +0530

    Added delete key route

--- src/routes/upload.route.ts
+++ src/routes/upload.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import pkg from "express-openid-connect";
-import { getPresignedUploadUrl } from "../controller/upload.controller.ts";
+import { deleteKey, getPresignedUploadUrl } from "../controller/upload.controller.ts";
 import { uploadValidaton } from "../middleware/validation.middleware.ts";
 
 const router = Router();
@@ -8,5 +8,6 @@ const router = Router();
 const { requiresAuth } = pkg;
 
 router.post("/presign", requiresAuth(), uploadValidaton, getPresignedUploadUrl);
+router.post("/deleteKey", requiresAuth, deleteKey);
 
 export default router;
