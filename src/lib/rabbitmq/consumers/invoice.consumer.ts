17cbea2f1fde1accc4b8a5b67e3e032ad715607b
ValoraSheikh <haarishsheikh04@gmail.com>
2026-06-22 16:44:18 +0530

    Implemented DLQ in invoice generation

--- src/lib/rabbitmq/consumers/invoice.consumer.ts
+++ src/lib/rabbitmq/consumers/invoice.consumer.ts
@@ -1,13 +1,23 @@
-import { connect } from "amqplib";
 import invoiceWorker from "../workers/invoice.worker.ts";
+import createRabbitMQConnection from "../config/connection.ts";
 
 async function invoiceConsumer() {
-  const connection = await connect(process.env.RABBITMQ_ACCESS_KEY || "");
-  const channel = await connection.createChannel();
+  const channel = await createRabbitMQConnection();
   channel.prefetch(1);
 
-  await channel.assertQueue("tribel.invoice.queue", { durable: true });
+  await channel.assertQueue("tribel.invoice.queue", {
+    durable: true,
+    arguments: {
+      "x-dead-letter-exchange": "tribel.dlx",
+      "x-dead-letter-routing-key": "invoice.failed",
+    },
+  });
   await channel.bindQueue("tribel.invoice.queue", "tribel.events", "invoice");
+
+  await channel.assertQueue("tribel.invoice.dlq", {
+    durable: true,
+  });
+  await channel.bindQueue("tribel.invoice.dlq", "tribel.dlx", "invoice.failed");
 
   await channel.consume(
     "tribel.invoice.queue",
@@ -16,10 +26,17 @@ async function invoiceConsumer() {
         const content = JSON.parse(msg.content.toString());
 
         console.log(" [x] Received '%s'", content);
-        const result = await invoiceWorker({ msg: content });
-        console.log(" [x] Result '%s'", result);
 
-        channel.ack(msg);
+        try {
+          const result = await invoiceWorker({ msg: content });
+          console.log(" [x] Result '%s'", result);
+          channel.ack(msg);
+        } catch (error) {
+          const message =
+            error instanceof Error ? error.message : String(error);
+          console.error(message);
+          channel.nack(msg, false, false);
+        }
       }
     },
     { noAck: false },
