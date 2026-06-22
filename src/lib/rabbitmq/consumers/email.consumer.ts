fc6a68945bedb1c7ed6ec28e2cbde47ae26e2f99
ValoraSheikh <haarishsheikh04@gmail.com>
2026-06-22 16:44:35 +0530

    Implemented DLQ in email generation

--- src/lib/rabbitmq/consumers/email.consumer.ts
+++ src/lib/rabbitmq/consumers/email.consumer.ts
@@ -1,13 +1,23 @@
-import { connect } from "amqplib";
 import emailWorker from "../workers/email.worker.ts";
+import createRabbitMQConnection from "../config/connection.ts";
 
 async function emailConsumer() {
-  const connection = await connect(process.env.RABBITMQ_ACCESS_KEY || "");
-  const channel = await connection.createChannel();
+  const channel = await createRabbitMQConnection();
   channel.prefetch(1);
 
-  await channel.assertQueue("tribel.email.queue", { durable: true });
+  await channel.assertQueue("tribel.email.queue", {
+    durable: true,
+    arguments: {
+      "x-dead-letter-exchange": "tribel.dlx",
+      "x-dead-letter-routing-key": "email.failed",
+    },
+  });
   await channel.bindQueue("tribel.email.queue", "tribel.events", "email");
+
+  await channel.assertQueue("tribel.email.dlq", {
+    durable: true,
+  });
+  await channel.bindQueue("tribel.email.dlq", "tribel.dlx", "email.failed");
 
   await channel.consume(
     "tribel.email.queue",
@@ -16,10 +26,17 @@ async function emailConsumer() {
         const content = JSON.parse(msg.content.toString());
 
         console.log(" [x] Received '%s'", content);
-        const result = await emailWorker({ msg: content });
-        console.log(" [x] Result '%s'", result);
 
-        channel.ack(msg);
+        try {
+          const result = await emailWorker({ msg: content });
+          console.log(" [x] Result '%s'", result);
+          channel.ack(msg);
+        } catch (error) {
+          const message =
+            error instanceof Error ? error.message : String(error);
+          console.error(message);
+          channel.nack(msg, false, false);
+        }
       }
     },
     { noAck: false },
