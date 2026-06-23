a4615493932c13adb851cfeadb736d091c4bbe71
ValoraSheikh <haarishsheikh04@gmail.com>
2026-06-23 13:05:59 +0530

    Implemented functionality for retry management in email created a retry queue which saves messages for 30 then put it back to main exchange and then implemented retry logic if retry excced max limit then publish message to dlx

--- src/lib/rabbitmq/consumers/email.consumer.ts
+++ src/lib/rabbitmq/consumers/email.consumer.ts
@@ -8,8 +8,8 @@ async function emailConsumer() {
   await channel.assertQueue("tribel.email.queue", {
     durable: true,
     arguments: {
-      "x-dead-letter-exchange": "tribel.dlx",
-      "x-dead-letter-routing-key": "email.failed",
+      "x-dead-letter-exchange": "tribel.retry",
+      "x-dead-letter-routing-key": "email.retry",
     },
   });
   await channel.bindQueue("tribel.email.queue", "tribel.events", "email");
@@ -19,22 +19,54 @@ async function emailConsumer() {
   });
   await channel.bindQueue("tribel.email.dlq", "tribel.dlx", "email.failed");
 
+  await channel.assertQueue("tribel.email.retry", {
+    durable: true,
+    arguments: {
+      "x-message-ttl": 30000,
+      "x-dead-letter-exchange": "tribel.events",
+      "x-dead-letter-routing-key": "email",
+    },
+  });
+  await channel.bindQueue("tribel.email.retry", "tribel.retry", "email.retry");
+
   await channel.consume(
     "tribel.email.queue",
     async (msg) => {
       if (msg) {
         const content = JSON.parse(msg.content.toString());
-
         console.log(" [x] Received '%s'", content);
 
         try {
+          const deathHeaders = msg.properties.headers?.["x-death"] as
+            | Array<{ queue: string; reason: string; count: number }>
+            | undefined;
+
+          const retries =
+            deathHeaders?.find(
+              (d) =>
+                d.queue === "tribel.email.queue" && d.reason === "rejected",
+            )?.count ?? 0;
+
+          if (retries >= 3) {
+            channel.publish(
+              "tribel.dlx",
+              "email.failed",
+              Buffer.from(JSON.stringify(content)),
+              {
+                persistent: true,
+              },
+            );
+
+            channel.ack(msg);
+          }
+
           const result = await emailWorker({ msg: content });
           console.log(" [x] Result '%s'", result);
           channel.ack(msg);
         } catch (error) {
-          const message =
+          const errorMessage =
             error instanceof Error ? error.message : String(error);
-          console.error(message);
+          console.error(errorMessage);
           channel.nack(msg, false, false);
         }
       }
