f34e96a8baf634234459799c2b3d10e2649f3da1
ValoraSheikh <haarishsheikh04@gmail.com>
2026-06-27 21:38:58 +0530

    Set new bookings to pending

--- src/controller/booking.controller.ts
+++ src/controller/booking.controller.ts
@@ -6,7 +6,8 @@ import { getSecuredClient } from "../lib/prisma/prisma-rls.ts";
 import client from "../lib/redis/redis-cache.ts";
 
 export const createBooking = asyncHandler(async (req, res) => {
-  const { propertyId, roomTemplateId, startDate, endDate, paymentMode } = req.body;
+  const { propertyId, roomTemplateId, startDate, endDate, paymentMode } =
+    req.body;
   let key: string | undefined;
   const keyValue = uuidv4();
   const ttl = 5000;
@@ -101,7 +102,7 @@ export const createBooking = asyncHandler(async (req, res) => {
             totalPrice: chooseBed?.pricePerBed,
             startDate,
             endDate,
-            status: "CONFIRMED",
+            status: "PENDING",
             paymentMode: paymentMode,
             paymentStatus: "PENDING",
           },
@@ -314,6 +315,8 @@ export const getUserBookings = asyncHandler(async (req, res) => {
         roomId: true,
         bedId: true,
         status: true,
+        paymentMode: true,
+        paymentStatus: true,
         guest: {
           select: {
             id: true,
@@ -462,6 +465,8 @@ export const getBookingsForAdmin = asyncHandler(async (req, res) => {
         roomId: true,
         bedId: true,
         status: true,
+        paymentMode: true,
+        paymentStatus: true,
         guest: {
           select: {
             id: true,
@@ -620,6 +625,8 @@ export const updateUserBooking = asyncHandler(async (req, res) => {
       roomId: true,
       bedId: true,
       status: true,
+      paymentMode: true,
+      paymentStatus: true,
       guest: {
         select: {
           id: true,
@@ -722,6 +729,8 @@ export const getAllBooking = asyncHandler(async (req, res) => {
         updatedAt: true,
         cancelledAt: true,
         bedId: true,
+        paymentMode: true,
+        paymentStatus: true,
         property: {
           select: {
             id: true,
@@ -851,6 +860,8 @@ export const getBookingDetails = asyncHandler(async (req, res) => {
       roomId: true,
       bedId: true,
       status: true,
+      paymentMode: true,
+      paymentStatus: true,
       guest: {
         select: {
           id: true,
